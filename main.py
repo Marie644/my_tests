e0eaf0ae7327b72030f077db887a44e8636ab43d
Mariia Karpenko <marielire644@gmail.com>
2020-08-29 18:43:45 +0300

    qqq

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ elem = driver.find_element_by_name("q")
 elem.clear()
 elem.send_keys("pyconon")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+assert "qweqwewq" not in driver.page_source
 driver.close()
